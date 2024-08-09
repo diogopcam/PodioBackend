f30098b01e577fb2953809c27edb4fad035269fa
diogpcam <diogpcam@gmail.com>
2024-08-08 22:05:03 -0300

    Melhora na organizacao do repositorio.

--- podio-backend/src/main/java/com/example/podio_backend/repositorios/RepositorioEsporte.java
+++
@@ -1,8 +0,0 @@
-package com.example.podio_backend.repositorios;
-
-import com.example.podio_backend.entidades.Esporte;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RepositorioEsporte extends MongoRepository<Esporte, Integer> {
-
-}
